8e9a9881ec28cb75b644acfd7f38743302f70072
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-09-11 14:42:36 -0300

    TikTok

---
+++ TikTok/src/main/java/com/tiktok/models/Funcionarios.java
@@ -0,0 +1,50 @@
+package com.tiktok.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Funcionarios implements Serializable{
+	
+	private static final long serialVersionUID = 1l;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	private String nome;
+	private String dataDeNascimento;
+	private String dataDeEntrada;
+	
+	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+	public void setDataDeNascimento(String dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+	public String getDataDeEntrada() {
+		return dataDeEntrada;
+	}
+	public void setDataDeEntrada(String dataDeEntrada) {
+		this.dataDeEntrada = dataDeEntrada;
+	}
+	
+	
+}
